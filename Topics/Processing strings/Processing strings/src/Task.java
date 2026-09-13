4214fd56e5689343391df8cc6475e7e769001950
Astal <bralves@hotmail.com.br>
2026-09-13 11:51:52 -0300

    Novos problemas resolvidos

--- Topics/Processing strings/Processing strings/src/Task.java
+++ Topics/Processing strings/Processing strings/src/Task.java
@@ -3,5 +3,16 @@
 public class Task {
   public static void main(String[] args) {
     // put your code here
+    String word = "racecar";
+    String reversedWord = "";
+    for (int i = word.length() - 1; i >= 0; i--){
+      reversedWord += word.charAt(i);
+    }
+    System.out.println(reversedWord);
+    if (word.equals(reversedWord)){
+      System.out.println("palindrome");
+    } else {
+      System.out.println("Not a palindrome");
+    }
   }
 }
