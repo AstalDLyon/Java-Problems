b52723c9827a02fd9754b8ffed5d8658bf7a1526
Astal <bralves@hotmail.com.br>
2026-09-15 06:25:38 -0300

    Novos problemas resolvidos

--- Topics/Processing strings/Double characters/src/Main.java
+++ Topics/Processing strings/Double characters/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String myString = scanner.nextLine();
+
+        String s = myString.replaceAll(".", "$0$0");
+        /*
+        The method String.replaceAll uses the regular expression syntax which is described in the documentation of
+        the Pattern class, where we can learn that . matches “any character”. Within the replacement,
+        $number refers to numbered “capturing group” whereas $0 is predefined as the entire match.
+        So $0$0 refers to the matching character two times. As the name of the method suggests,
+         it is performed for all matches, i.e. all characters.
+        */
+        System.out.println(s);
+        // OU SE PRECISAR FAZER NA MÃO, USE UMA VARIAVEL TEMPORARIA PARA TRANSFERENCIA:
+        String result = "";
+        for (int i = 0; i < myString.length(); i++ ){
+            char c = myString.charAt(i);
+            result = result + c + c; // result = "" + c + c; tbm funciona
+        }
+        System.out.println(result);
     }
 }
