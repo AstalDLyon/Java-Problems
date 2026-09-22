f15ca42b8c4554334f3da19d5ec5125e51d3b716
Astal <bralves@hotmail.com.br>
2026-09-22 15:21:23 -0300

    novas tarefas

---
+++ Topics/java.util.Arrays/Convert the array to a stream/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = {scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
+        int sum = Arrays.stream(arr).sum();
+
+        // write your code here
+        System.out.println(sum);
+    }
+}
